ae91cc3d2837f18a78f4d6d71582cdffbe7f8700
William Asaba <asabawilliamdk@yahoo.com>
2026-02-15 13:25:42 +0300

    refactor: Simplify service provider
    Removed installation command registration and simplified component loading.

--- src/SearchableSelectServiceProvider.php
+++ src/SearchableSelectServiceProvider.php
@@ -3,36 +3,28 @@
 namespace Williamug\SearchableSelect;
 
 use Illuminate\Support\ServiceProvider;
-use Williamug\SearchableSelect\Commands\InstallSearchableSelectCommand;
 
 class SearchableSelectServiceProvider extends ServiceProvider
 {
-    /**
-     * Register services.
-     */
-    public function register(): void
-    {
-        //
-    }
+  /**
+   * Register services.
+   */
+  public function register(): void
+  {
+    //
+  }
 
-    /**
-     * Bootstrap services.
-     */
-    public function boot(): void
-    {
-        // Register the installation command
-        if ($this->app->runningInConsole()) {
-            $this->commands([
-                InstallSearchableSelectCommand::class,
-            ]);
-        }
+  /**
+   * Bootstrap services.
+   */
+  public function boot(): void
+  {
+    // Load views from the package
+    $this->loadViewsFrom(__DIR__ . '/../resources/views', 'searchable-select');
 
-        // Publish the component view
-        $this->publishes([
-            __DIR__.'/../resources/views/searchable-select.blade.php' => resource_path('views/components/searchable-select.blade.php'),
-        ], 'searchable-select-component');
-
-        // Load views from the package
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'searchable-select');
-    }
+    // Register Blade component for <x-searchable-select /> syntax
+    $this->loadViewComponentsAs('', [
+      'searchable-select' => \Williamug\SearchableSelect\View\Components\SearchableSelect::class,
+    ]);
+  }
 }
