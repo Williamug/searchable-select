9a47c3437868929f3570dc3aea9f61f3e43e9582
William Asaba <asabawilliamdk@yahoo.com>
2026-02-15 13:41:47 +0300

    refactor: Remove unused component class
    Removed component class since we're using anonymous component approach.

--- src/View/Components/SearchableSelect.php
+++ src/View/Components/SearchableSelect.php
@@ -2,16 +2,15 @@
 
 namespace Williamug\SearchableSelect\View\Components;
 
-use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
 class SearchableSelect extends Component
 {
-  /**
-   * Get the view / contents that represent the component.
-   */
-  public function render(): View
-  {
-    return view('searchable-select::searchable-select');
-  }
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render()
+    {
+        return view('searchable-select::searchable-select');
+    }
 }
