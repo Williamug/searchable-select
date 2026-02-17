5c2e2a75c3346e632f00dea9a1ab2dac3aaf7ebe
William Asaba <asabawilliamdk@yahoo.com>
2026-02-17 15:16:19 +0300

    feat: add Bootstrap demo component
    Created Livewire component to demonstrate Bootstrap 5 theme integration with searchable-select component.

---
+++ demo/app/Livewire/BootstrapDemo.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class BootstrapDemo extends Component
+{
+  public $countries;
+  public $country_id;
+  public $selected_cities = [];
+
+  public function mount()
+  {
+    $this->countries = [
+      ['id' => 1, 'name' => 'United States'],
+      ['id' => 2, 'name' => 'Canada'],
+      ['id' => 3, 'name' => 'United Kingdom'],
+      ['id' => 4, 'name' => 'Australia'],
+      ['id' => 5, 'name' => 'Germany'],
+    ];
+  }
+
+  public function render()
+  {
+    return view('livewire.bootstrap-demo');
+  }
+}
