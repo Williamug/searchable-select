35cb7ee236e700cc15eeb7ca7e17dd998c388e4e
William Asaba <asabawilliamdk@yahoo.com>
2026-02-17 16:56:23 +0300

    wip

--- src/SearchableSelectServiceProvider.php
+++ src/SearchableSelectServiceProvider.php
@@ -7,34 +7,34 @@ use Illuminate\Support\ServiceProvider;
 
 class SearchableSelectServiceProvider extends ServiceProvider
 {
-    /**
-     * Register services.
-     */
-    public function register(): void
-    {
-        // Merge config
-        $this->mergeConfigFrom(
-            __DIR__.'/../config/searchable-select.php',
-            'searchable-select'
-        );
+  /**
+   * Register services.
+   */
+  public function register(): void
+  {
+    // Merge config
+    $this->mergeConfigFrom(
+      __DIR__ . '/../config/searchable-select.php',
+      'searchable-select'
+    );
+  }
+
+  /**
+   * Bootstrap services.
+   */
+  public function boot(): void
+  {
+    // Load views from the package
+    $this->loadViewsFrom(__DIR__ . '/../resources/views', 'searchable-select');
+
+    // Register the searchable-select view as an anonymous component
+    Blade::component('searchable-select::searchable-select', 'searchable-select');
+
+    // Publish config
+    if ($this->app->runningInConsole()) {
+      $this->publishes([
+        __DIR__ . '/../config/searchable-select.php' => config_path('searchable-select.php'),
+      ], 'searchable-select-config');
     }
-
-    /**
-     * Bootstrap services.
-     */
-    public function boot(): void
-    {
-        // Load views from the package
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'searchable-select');
-
-        // Register the searchable-select view as an anonymous component
-        Blade::component('searchable-select::searchable-select', 'searchable-select');
-
-        // Publish config
-        if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__.'/../config/searchable-select.php' => config_path('searchable-select.php'),
-            ], 'searchable-select-config');
-        }
-    }
+  }
 }
