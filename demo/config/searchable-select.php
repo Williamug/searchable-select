5a9976ddbe0eb9409e67a1f02a518cfb2a950f70
William Asaba <asabawilliamdk@yahoo.com>
2026-02-17 15:16:23 +0300

    chore: add demo configuration
    Added searchable-select configuration file for the demo application.

---
+++ demo/config/searchable-select.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+  /*
+    |--------------------------------------------------------------------------
+    | Default Theme
+    |--------------------------------------------------------------------------
+    |
+    | This option controls the default CSS framework used for styling the
+    | searchable select component. Supported values: "tailwind", "bootstrap"
+    |
+    */
+
+  'theme' => env('SEARCHABLE_SELECT_THEME', 'bootstrap'),  /*
+    |--------------------------------------------------------------------------
+    | Bootstrap Version
+    |--------------------------------------------------------------------------
+    |
+    | Specify the Bootstrap version you're using. This helps optimize
+    | the CSS classes for your specific Bootstrap version.
+    | Supported values: "5"
+    |
+    */
+
+  'bootstrap_version' => env('SEARCHABLE_SELECT_BOOTSTRAP_VERSION', '5'),
+];
