59a11344e4cb02e495fa4c7fc6ffe3d018d946c4
William Asaba <asabawilliamdk@yahoo.com>
2026-04-26 20:53:23 +0300

    feat: Add Bootstrap demo Livewire component
    Created new BootstrapDemo component to demonstrate
    searchable-select functionality with Bootstrap styling.

--- demo/app/Livewire/BootstrapDemo.php
+++ demo/app/Livewire/BootstrapDemo.php
@@ -6,11 +6,17 @@ use Livewire\Component;
 
 class BootstrapDemo extends Component
 {
-    public $countries;
-
     public $country_id;
 
-    public $selected_cities = [];
+    public $selected_countries = [];
+
+    public $region_id;
+
+    public $api_country_id;
+
+    public $countries;
+
+    public $groupedRegions;
 
     public function mount()
     {
@@ -20,6 +26,52 @@ class BootstrapDemo extends Component
             ['id' => 3, 'name' => 'United Kingdom'],
             ['id' => 4, 'name' => 'Australia'],
             ['id' => 5, 'name' => 'Germany'],
+            ['id' => 6, 'name' => 'France'],
+            ['id' => 7, 'name' => 'Italy'],
+            ['id' => 8, 'name' => 'Spain'],
+            ['id' => 9, 'name' => 'Japan'],
+            ['id' => 10, 'name' => 'Brazil'],
+            ['id' => 11, 'name' => 'India'],
+            ['id' => 12, 'name' => 'South Africa'],
+        ];
+
+        $this->groupedRegions = [
+            [
+                'label' => 'Americas',
+                'options' => [
+                    ['id' => 1, 'name' => 'United States'],
+                    ['id' => 2, 'name' => 'Canada'],
+                    ['id' => 3, 'name' => 'Brazil'],
+                    ['id' => 4, 'name' => 'Mexico'],
+                ],
+            ],
+            [
+                'label' => 'Europe',
+                'options' => [
+                    ['id' => 5, 'name' => 'United Kingdom'],
+                    ['id' => 6, 'name' => 'Germany'],
+                    ['id' => 7, 'name' => 'France'],
+                    ['id' => 8, 'name' => 'Italy'],
+                    ['id' => 9, 'name' => 'Spain'],
+                ],
+            ],
+            [
+                'label' => 'Asia Pacific',
+                'options' => [
+                    ['id' => 10, 'name' => 'Japan'],
+                    ['id' => 11, 'name' => 'Australia'],
+                    ['id' => 12, 'name' => 'India'],
+                    ['id' => 13, 'name' => 'China'],
+                ],
+            ],
+            [
+                'label' => 'Africa',
+                'options' => [
+                    ['id' => 14, 'name' => 'South Africa'],
+                    ['id' => 15, 'name' => 'Nigeria'],
+                    ['id' => 16, 'name' => 'Kenya'],
+                ],
+            ],
         ];
     }
 
